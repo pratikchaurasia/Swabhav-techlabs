30d9086750ba608839e595c66329fbb2c6d76cec
Pratik Chaurasia <pratikchaurasia@outlook.com>
2017-12-26 18:47:06 +0530

    refactor code

--- Core Java/basic-app/src/com/techlabs/array/TestWelcome.java
+++ Core Java/basic-app/src/com/techlabs/array/TestWelcome.java
@@ -1,8 +1,16 @@
 package com.techlabs.array;
 
 public class TestWelcome {
-public static void main(String []args)
-{
-	System.out.printf("Welcome");
-}
+	public static void main(String[] names) {
+
+		if (names.length == 0) {
+			System.out.println("Pass some names");
+		}
+
+		else {
+			for (String n : names) {
+				System.out.println(n);
+			}
+		}
+	}
 }
